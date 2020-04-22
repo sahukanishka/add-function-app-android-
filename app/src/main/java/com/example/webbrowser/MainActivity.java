ccf6e39c5bd560d978058e09b26ed3e9b7606ca7
kanishkasahu <kanishkasahu005@gmail.com>
2020-04-23 01:43:17 +0530

    first commit on android

---
+++ app/src/main/java/com/example/webbrowser/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.webbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView= (WebView)findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.loadUrl(“https://www.google.co.in/”);
+
+    }
+}
